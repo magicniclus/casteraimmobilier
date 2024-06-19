c9a2b115f63cade142b2d217e20327350c79fb62
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-19 18:31:49 +0200

    Create Connexion.tsx

---
+++ src/components/tailwindUi/form/Connexion.tsx
@@ -0,0 +1,143 @@
+/* eslint-disable @next/next/no-img-element */
+
+"use client";
+
+import { Toaster } from "@/components/ui/sonner"; // Importez votre composant Toaster
+import { signUp } from "@/firebase/auth"; // Importez la fonction signUp depuis son chemin
+import { useState } from "react";
+import { toast } from "sonner"; // Assurez-vous que l'importation est correcte
+
+const ConnexionForm: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password: string): boolean => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("L'adresse email n'est pas valide.");
+      toast.error("L'adresse email n'est pas valide."); // Afficher le message d'erreur avec Toaster
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setError(
+        "Le mot de passe doit contenir au moins 8 caractères, dont une lettre et un chiffre."
+      );
+      toast.error(
+        "Le mot de passe doit contenir au moins 8 caractères, dont une lettre et un chiffre."
+      ); // Afficher le message d'erreur avec Toaster
+      return;
+    }
+
+    try {
+      await signUp(email, password);
+      // Redirection ou autres actions après connexion réussie
+    } catch (error) {
+      setError("Échec de la connexion. Veuillez vérifier vos informations.");
+      toast.error("Échec de la connexion. Veuillez vérifier vos informations."); // Afficher le message d'erreur avec Toaster
+    }
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <Toaster /> {/* Assurez-vous que le composant Toaster est inclus */}
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Connectez-vous à votre espace
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Adresse email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Mot de passe
+              </label>
+              <div className="text-sm">
+                <a
+                  href="/user/changer-le-mot-de-passe"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Mot de passe oublié ?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Se connecter
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Pas encore inscrit ?{" "}
+          <a
+            href="#"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Publiez votre projet
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ConnexionForm;
