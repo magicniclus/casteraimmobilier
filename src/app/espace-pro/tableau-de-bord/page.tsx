4eb4ed337aae9a8396b1f42a664a70ba5728fb07
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-20 17:58:36 +0200

    Create page.tsx

---
+++ src/app/espace-pro/tableau-de-bord/page.tsx
@@ -0,0 +1,11 @@
+import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+
+const Page = () => {
+  return (
+    <SideBarLayout>
+      <h1>Tableau de bord</h1>
+    </SideBarLayout>
+  );
+};
+
+export default Page;
