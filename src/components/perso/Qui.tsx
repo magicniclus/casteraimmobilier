e731b4be17011eb1b8f3020a47ba13487c83e024
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-10-09 17:21:54 +0200

    Create Qui.tsx

---
+++ src/components/perso/Qui.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+const Qui = () => {
+  return (
+    <section className="w-full">
+      <div className="w-full max-w-7xl mx-auto py-32">
+        <div className="flex">
+          <div className="relative flex items-center">
+            <img
+              src="https://files.oaiusercontent.com/file-ZRtNOkA2DSl8nkZNBPRWqmI2?se=2024-10-09T15%3A08%3A16Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D2b7d00ed-c3c2-4f9d-b91a-47170b8ce5e6.webp&sig=/Zv6F4kcw9RYh3tXffpM7k0%2B6F8t5BS/4/ogGi3Q81w%3D"
+              alt="Nicolas CASTERA"
+              className="h-[700px] object-cover w-5/12 rounded-l-[100px] rounded-r-lg"
+            />
+            <div className="w-7/12 bg-[#0f1618] text-white py-20 px-10 h-max rounded-r-[100px]">
+              <h2 className="text-4xl">Rencontrez Nicolas Castera</h2>
+              <p className="mt-10">
+                Avec plus de 2 milliards de dollars de ventes, notre agence est
+                le premier producteur de luxe du secteur avec plus de 27 ans
+                d&apos;expérience dans la commercialisation des propriétés
+                riveraines les plus prestigieuses de Seattle.
+              </p>
+              <p className="mt-8">
+                Avec plus de 2 milliards de dollars de ventes, notre agence est
+                le premier producteur de luxe du secteur avec plus de 27 ans
+                d&apos;expérience dans la commercialisation des propriétés
+                riveraines les plus prestigieuses de Seattle.
+              </p>
+              <h3 className="text-4xl text-[#947a2f] mt-10 font-light font-updock">
+                Nicolas Castera
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qui;
