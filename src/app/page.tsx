a9c454e4f6d993fc30a3d0291e3de2a6fb14c3e4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-10-09 17:21:50 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Qui from "@/components/perso/Qui";
 import Selection from "@/components/perso/Selection";
 import Footer from "@/components/tailwindUi/footer/Footer";
 import Header from "@/components/tailwindUi/header/Header";
@@ -7,9 +8,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center -z-10">
+      <main className="flex min-h-screen flex-col items-center -z-10 bg-[#fbf9f7]">
         <Hero />
         <Selection />
+        <Qui />
       </main>
       <Footer />
     </>
