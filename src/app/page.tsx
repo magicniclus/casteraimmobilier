f6838ca318d8b6bcad4d4569f777ae6f87c8ae39
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-10-11 14:32:42 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
+"use client";
+
+import Paralax from "@/components/perso/Paralax";
 import Qui from "@/components/perso/Qui";
 import Selection from "@/components/perso/Selection";
 import Footer from "@/components/tailwindUi/footer/Footer";
 import Header from "@/components/tailwindUi/header/Header";
 import Hero from "@/components/tailwindUi/hero/Hero";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const handleSmoothScroll = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.matches("a[href^='#']")) {
+        event.preventDefault();
+        const id = target.getAttribute("href")?.slice(1);
+        if (id) {
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      }
+    };
+
+    document.addEventListener("click", handleSmoothScroll);
+
+    return () => {
+      document.removeEventListener("click", handleSmoothScroll);
+    };
+  }, []);
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center -z-10 bg-[#fbf9f7]">
         <Hero />
         <Selection />
-        {/* <Paralax /> */}
+        <Paralax />
         <Qui />
       </main>
       <Footer />
